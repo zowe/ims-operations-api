9fee7509535a0f34e7832c25f97b623eb1986d57
lij <lij@us.ibm.com>
2018-12-06 16:10:40 -0800

    change name

--- imscmdapi/src/test/java/zowe/mc/MCSuiteTest.java
+++ imscmdapi/src/test/java/zowe/mc/MCSuiteTest.java
@@ -3,8 +3,7 @@ package zowe.mc;
 import org.junit.platform.runner.JUnitPlatform;
 import org.junit.platform.suite.api.SelectClasses;
 import org.junit.runner.RunWith;
-//import org.junit.runners.Suite;
-//import org.junit.runners.Suite.SuiteClasses;
+
 
 import zowe.mc.tests.TestOMConnection;
 import zowe.mc.tests.pgm.TestCreateDeletePgm;
@@ -13,6 +12,13 @@ import zowe.mc.tests.pgm.TestStartPgm;
 import zowe.mc.tests.pgm.TestUpdatePgm;
 import zowe.mc.tests.tran.TestQueryTran;
 
+/**
+ * Right now we have to use JUnit 4 runner for test suites because test suites are
+ * currently not supported in JUnit 5. Wondering if we should get rid of test suites 
+ * since Maven runs all of the tests during build anyway.
+ * @author jerryli
+ *
+ */
 @RunWith(JUnitPlatform.class)
 @SelectClasses( { TestOMConnection.class, 
 				  TestQueryPgm.class , 
