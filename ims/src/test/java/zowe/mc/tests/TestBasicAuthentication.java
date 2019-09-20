47b73e17a4f2d11e2c68998d023fd013c077e768
Omar Elbarmawi <omar@ibm.com>
2019-09-19 18:24:40 -0700

    Finished Region Unit Tests

--- ims/src/test/java/zowe/mc/tests/TestBasicAuthentication.java
+++ ims/src/test/java/zowe/mc/tests/TestBasicAuthentication.java
@@ -163,7 +163,7 @@ public class TestBasicAuthentication {
 		List<String[]> queryParams = new ArrayList<>();
 		String[] names = new String[] {"name", "JUNIT"};
 		queryParams.add(names);
-		RequestUtils.postRequest(queryParams, PGM_PATH, PGM_USER, DEFAULT_PASSWORD);
+		RequestUtils.postRequest(queryParams, PGM_PATH, ADMIN_USER, DEFAULT_PASSWORD);
 		
 		// Attempt to create transaction
 		logger.info("Attempting to create test transaction");
@@ -195,6 +195,113 @@ public class TestBasicAuthentication {
 		logger.info("TESTING Pgm User Access Region PUT Service");
 		Response response = RequestUtils.putRequest(new ArrayList<String[]>(), REGION_PATH + "/stop", PGM_USER, DEFAULT_PASSWORD);
 		assertEquals(Response.Status.FORBIDDEN.getStatusCode(), response.getStatus());
+	}
+	
+	@Test
+	public void testRegionUserAccessRegionGetService() {
+		logger.info("TESTING Region User Access Region GET Service");
+		Response response = RequestUtils.getRequest(new ArrayList<String[]>(), REGION_PATH, REGION_USER, DEFAULT_PASSWORD);
+		assertEquals(Response.Status.OK.getStatusCode(), response.getStatus());
+	}
+	
+	@Test
+	public void testRegionUserAccessRegionPutService() {
+		logger.info("TESTING Region User Access Region PUT Service");
+		Response response = RequestUtils.putRequest(new ArrayList<String[]>(), REGION_PATH + "/stop", REGION_USER, DEFAULT_PASSWORD);
+		assertEquals(Response.Status.OK.getStatusCode(), response.getStatus());
+	}
+	
+	@Test
+	public void testRegionUserAccessPgmGetService() {
+		logger.info("TESTING REGION User Access Region GET Service");
+		Response response = RequestUtils.getRequest(new ArrayList<String[]>(), PGM_PATH, REGION_USER, DEFAULT_PASSWORD);
+		assertEquals(Response.Status.FORBIDDEN.getStatusCode(), response.getStatus());
+	}
+	
+	@Test
+	public void testRegionUserAccessPgmPutService() {
+		logger.info("TESTING Region User Access Pgm PUT Service");
+		
+		List<String[]> queryParams = new ArrayList<>();
+		String[] names = new String[] {"name", "DBF*"};
+		String[] stop = new String[] {"stop", "SCHD"};
+		queryParams.add(names);
+		queryParams.add(stop);
+		
+		Response response = RequestUtils.putRequest(queryParams, PGM_PATH, REGION_USER, DEFAULT_PASSWORD);
+		assertEquals(Response.Status.FORBIDDEN.getStatusCode(), response.getStatus());
+	}
+	
+	@Test
+	public void testRegionUserAccessPgmPostService() {
+		logger.info("TESTING Region User Access Pgm POST Service");
+		/* 
+		 * For Pgm POST service, we must create and delete the resource that was created
+		 * NOTE: Using ADMIN_USER for portions of the test that require deleting resources.
+		 *  
+		 * */
+		
+		/* Delete the TEST pgm if already created */
+		List<String[]> queryParamspre = new ArrayList<>();
+		String[] namespre = new String[] {"name", "TEST"};
+		queryParamspre.add(namespre);
+		RequestUtils.deleteRequest(queryParamspre, PGM_PATH, ADMIN_USER, DEFAULT_PASSWORD);
+		
+		/* Attempt to create the TEST pgm */
+		List<String[]> queryParams = new ArrayList<>();
+		String[] names = new String[] {"name", "TEST"};
+		queryParams.add(names);
+		Response response = RequestUtils.postRequest(queryParams, PGM_PATH, REGION_USER, DEFAULT_PASSWORD);
+		assertEquals(Response.Status.FORBIDDEN.getStatusCode(), response.getStatus());
+	}
+	
+	@Test
+	public void testRegionUserAccessTranGetService() {
+		logger.info("TESTING Region User Access Tran GET Service");
+		Response response = RequestUtils.getRequest(new ArrayList<String[]>(), TRAN_PATH, REGION_USER, DEFAULT_PASSWORD);
+		assertEquals(Response.Status.FORBIDDEN.getStatusCode(), response.getStatus());
+	}
+	
+	@Test
+	public void testRegionUserAccessTranPutService() {
+		logger.info("TESTING Region User Access Tran PUT Service (stopping scheduling of a transaction)");		
+		List<String[]> queryParams = new ArrayList<>();
+		String[] names = new String[] {"name", "JUNIT"};
+		String[] stop = new String[] {"stop", "SCHD"};
+		queryParams.add(names);
+		queryParams.add(stop);
+		Response response = RequestUtils.putRequest(queryParams, TRAN_PATH, REGION_USER, DEFAULT_PASSWORD);
+		assertEquals(Response.Status.FORBIDDEN.getStatusCode(), response.getStatus());
+	}
+	
+	@Test
+	public void testRegionUserAccessTranPostService() {
+		logger.info("TESTING Pgm User Access Tran POST Service");	
+		/* Will use ADMIN_USER for any necessary delete requests */
+		
+		// Need to create a program first
+		logger.info("Creating test program 'JUNIT'");
+		List<String[]> queryParams = new ArrayList<>();
+		String[] names = new String[] {"name", "JUNIT"};
+		queryParams.add(names);
+		RequestUtils.postRequest(queryParams, PGM_PATH, ADMIN_USER, DEFAULT_PASSWORD);
+		
+		// Attempt to create transaction
+		logger.info("Attempting to create test transaction");
+		List<String[]> queryParams1 = new ArrayList<>();
+		String[] names1 = new String[] {"name", "TEST"};
+		String[] pgm1 = new String[] {"pgm", "JUNIT"};
+		queryParams1.add(names1);
+		queryParams1.add(pgm1);
+		Response response1 = RequestUtils.postRequest(queryParams1, TRAN_PATH, REGION_USER, DEFAULT_PASSWORD);
+		assertEquals(Response.Status.FORBIDDEN.getStatusCode(), response1.getStatus());
+		
+		// Delete program that was created
+		logger.info("Deleting test program 'JUNIT'");
+		List<String[]> queryParams3 = new ArrayList<>();
+		String[] names3 = new String[] {"name", "JUNIT"};
+		queryParams3.add(names3);
+		RequestUtils.deleteRequest(queryParams3, PGM_PATH, ADMIN_USER, DEFAULT_PASSWORD);
 	}
 	
 	
