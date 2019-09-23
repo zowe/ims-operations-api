2cf2cb902cee5e642dfa99751c49bf51f600d027
Omar Elbarmawi <omar@ibm.com>
2019-09-23 11:32:51 -0700

    Added Post User Unit Tests (DeleteRequests Failing)

--- ims/src/test/java/zowe/mc/tests/TestBasicAuthentication.java
+++ ims/src/test/java/zowe/mc/tests/TestBasicAuthentication.java
@@ -213,7 +213,7 @@ public class TestBasicAuthentication {
 	
 	@Test
 	public void testRegionUserAccessPgmGetService() {
-		logger.info("TESTING REGION User Access Region GET Service");
+		logger.info("TESTING REGION User Access Pgm GET Service");
 		Response response = RequestUtils.getRequest(new ArrayList<String[]>(), PGM_PATH, REGION_USER, DEFAULT_PASSWORD);
 		assertEquals(Response.Status.FORBIDDEN.getStatusCode(), response.getStatus());
 	}
@@ -413,7 +413,7 @@ public class TestBasicAuthentication {
 	
 	@Test
 	public void testGetUserAccessPgmDeleteService() {
-		logger.info("TESTING Get User Access Tran DELETE Service");	
+		logger.info("TESTING Get User Access Pgm DELETE Service");	
 		// Will use ADMIN_USER for any necessary delete requests
 		// Create a program first
 		logger.info("Creating test program 'JUNIT'");
@@ -449,7 +449,7 @@ public class TestBasicAuthentication {
 		queryParams.add(names);
 		RequestUtils.postRequest(queryParams, PGM_PATH, ADMIN_USER, DEFAULT_PASSWORD);
 		
-		// Attempt to create transaction
+		// Create transaction
 		logger.info("Create test transaction");
 		List<String[]> queryParams1 = new ArrayList<>();
 		String[] names1 = new String[] {"name", "TEST"};
@@ -501,7 +501,227 @@ public class TestBasicAuthentication {
 		RequestUtils.deleteRequest(queryParams3, PGM_PATH, ADMIN_USER, DEFAULT_PASSWORD);
 	}
 	
+	@Test
+	public void testPostUserAccessPgmPostService() {
+		logger.info("TESTING Post User Access Pgm POST Service");	
+		// Will use ADMIN_USER for any necessary delete requests
+		// Need to create a program first
+		logger.info("Creating test program 'JUNIT'");
+		List<String[]> queryParams = new ArrayList<>();
+		String[] names = new String[] {"name", "JUNIT"};
+		queryParams.add(names);
+		Response response = RequestUtils.postRequest(queryParams, PGM_PATH, POST_USER, DEFAULT_PASSWORD);
+		assertEquals(Response.Status.OK.getStatusCode(), response.getStatus());
+		
+		// Delete program that was created
+		logger.info("Deleting test program 'JUNIT'");
+		List<String[]> queryParams3 = new ArrayList<>();
+		String[] names3 = new String[] {"name", "JUNIT"};
+		queryParams3.add(names3);
+		RequestUtils.deleteRequest(queryParams3, PGM_PATH, ADMIN_USER, DEFAULT_PASSWORD);
+	}
 	
+	@Test
+	public void testPostUserAccessTranPostService() {
+		logger.info("TESTING Post User Access Tran POST Service");	
+		// Will use ADMIN_USER for any necessary delete requests
+		// Need to create a program first
+		logger.info("Creating test program 'JUNIT'");
+		List<String[]> queryParams = new ArrayList<>();
+		String[] names = new String[] {"name", "JUNIT"};
+		queryParams.add(names);
+		RequestUtils.postRequest(queryParams, PGM_PATH, ADMIN_USER, DEFAULT_PASSWORD);
+		
+		// Create transaction
+		logger.info("Create test transaction");
+		List<String[]> queryParams1 = new ArrayList<>();
+		String[] names1 = new String[] {"name", "TEST"};
+		String[] pgm1 = new String[] {"pgm", "JUNIT"};
+		queryParams1.add(names1);
+		queryParams1.add(pgm1);
+		Response response1 = RequestUtils.postRequest(queryParams1, TRAN_PATH, POST_USER, DEFAULT_PASSWORD);
+		assertEquals(Response.Status.OK.getStatusCode(), response1.getStatus());
+		CreateTransactionOutput ctr = RequestUtils.validateCTRSuccess(response1);
+		/*Check if data is correct*/
+		logger.info(ctr.toString());
+		for (CreateTransaction q : ctr.getData()) {
+			assertEquals("0", q.getCc());
+			assertEquals("TEST", q.getTran());
+		}
+		for (String key : ctr.getMessages().keySet()) {
+			assertEquals(null, ctr.getMessages().get(key).getRc());
+		}
+		
+		// Delete the transaction
+		logger.info("Deleting test transaction");
+		List<String[]> queryParams4 = new ArrayList<>();
+		String[] names4 = new String[] {"name", "TEST"};
+		queryParams4.add(names4);
+		Response response4 = RequestUtils.deleteRequest(queryParams4, TRAN_PATH, ADMIN_USER, DEFAULT_PASSWORD);
+		DeleteTransactionOutput dtr4 = RequestUtils.validateDTRSuccess(response4);
+		/*Check if data is correct*/
+		logger.info(dtr4.toString());
+		for (DeleteTransaction q : dtr4.getData()) {
+			assertEquals("0", q.getCc());
+			assertEquals("TEST", q.getTran());
+		}
+		for (String key : dtr4.getMessages().keySet()) {
+			assertEquals(null, dtr4.getMessages().get(key).getRc());
+		}
+		
+		// Delete program that was created
+		logger.info("Deleting test program 'JUNIT'");
+		List<String[]> queryParams3 = new ArrayList<>();
+		String[] names3 = new String[] {"name", "JUNIT"};
+		queryParams3.add(names3);
+		RequestUtils.deleteRequest(queryParams3, PGM_PATH, ADMIN_USER, DEFAULT_PASSWORD);
+	}
+	
+	@Test
+	public void testPostUserAccessPgmGetService() {
+		logger.info("TESTING Post User Access Pgm GET Service");
+		Response response = RequestUtils.getRequest(new ArrayList<String[]>(), PGM_PATH, POST_USER, DEFAULT_PASSWORD);
+		assertEquals(Response.Status.FORBIDDEN.getStatusCode(), response.getStatus());
+	}
+	
+	@Test
+	public void testPostUserAccessTranGetService() {
+		logger.info("TESTING Post User Access Tran GET Service");
+		Response response = RequestUtils.getRequest(new ArrayList<String[]>(), TRAN_PATH, POST_USER, DEFAULT_PASSWORD);
+		assertEquals(Response.Status.FORBIDDEN.getStatusCode(), response.getStatus());
+	}
+	
+	@Test
+	public void testPostUserAccessRegionGetService() {
+		logger.info("TESTING Post User Access Region GET Service");
+		Response response = RequestUtils.getRequest(new ArrayList<String[]>(), REGION_PATH, POST_USER, DEFAULT_PASSWORD);
+		assertEquals(Response.Status.FORBIDDEN.getStatusCode(), response.getStatus());
+	}
+	
+	@Test
+	public void testPostUserAccessPgmPutService() {
+		logger.info("TESTING Post User Access Pgm PUT Service");
+		
+		List<String[]> queryParams = new ArrayList<>();
+		String[] names = new String[] {"name", "DBF*"};
+		String[] stop = new String[] {"stop", "SCHD"};
+		queryParams.add(names);
+		queryParams.add(stop);
+		
+		Response response = RequestUtils.putRequest(queryParams, PGM_PATH, POST_USER, DEFAULT_PASSWORD);
+		assertEquals(Response.Status.FORBIDDEN.getStatusCode(), response.getStatus());
+	}
+	
+	@Test
+	public void testPostUserAccessTranPutService() {
+		logger.info("TESTING Post User Access Tran PUT Service (stopping scheduling of a transaction)");		
+		List<String[]> queryParams = new ArrayList<>();
+		String[] names = new String[] {"name", "JUNIT"};
+		String[] stop = new String[] {"stop", "SCHD"};
+		queryParams.add(names);
+		queryParams.add(stop);
+		Response response = RequestUtils.putRequest(queryParams, TRAN_PATH, POST_USER, DEFAULT_PASSWORD);
+		assertEquals(Response.Status.FORBIDDEN.getStatusCode(), response.getStatus());
+	}
+	
+	@Test
+	public void testPostUserAccessRegionPutService() {
+		logger.info("TESTING Post User Access Region PUT Service");
+		Response response = RequestUtils.putRequest(new ArrayList<String[]>(), REGION_PATH + "/stop", POST_USER, DEFAULT_PASSWORD);
+		assertEquals(Response.Status.FORBIDDEN.getStatusCode(), response.getStatus());
+	}
+	/*(
+	@Test
+	public void testPostUserAccessPgmDeleteService() {
+		logger.info("TESTING Post User Access Pgm DELETE Service");	
+		// Will use ADMIN_USER for any necessary delete requests
+		// Create a program first
+		logger.info("Creating test program 'JUNIT'");
+		List<String[]> queryParams = new ArrayList<>();
+		String[] names = new String[] {"name", "JUNIT"};
+		queryParams.add(names);
+		RequestUtils.postRequest(queryParams, PGM_PATH, ADMIN_USER, DEFAULT_PASSWORD);
+		
+		// Attempt to delete test program JUNIT
+		logger.info("Attempting to delete test program 'JUNIT'");
+		List<String[]> queryParams2 = new ArrayList<>();
+		String[] names2 = new String[] {"name", "JUNIT"};
+		queryParams2.add(names2);
+		Response response = RequestUtils.deleteRequest(queryParams2, PGM_PATH, POST_USER, DEFAULT_PASSWORD);
+		assertEquals(Response.Status.FORBIDDEN.getStatusCode(), response.getStatus());
+		
+		// Delete program that was created
+		logger.info("Deleting test program 'JUNIT'");
+		List<String[]> queryParams3 = new ArrayList<>();
+		String[] names3 = new String[] {"name", "JUNIT"};
+		queryParams3.add(names3);
+		RequestUtils.deleteRequest(queryParams3, PGM_PATH, ADMIN_USER, DEFAULT_PASSWORD);
+	}
+	
+	@Test
+	public void testPostUserAccessTranDeleteService() {
+		logger.info("TESTING Post User Access Tran DELETE Service");	
+		// Will use ADMIN_USER for any necessary delete requests
+		// Need to create a program first
+		logger.info("Creating test program 'JUNIT'");
+		List<String[]> queryParams = new ArrayList<>();
+		String[] names = new String[] {"name", "JUNIT"};
+		queryParams.add(names);
+		RequestUtils.postRequest(queryParams, PGM_PATH, ADMIN_USER, DEFAULT_PASSWORD);
+		
+		// Create transaction
+		logger.info("Create test transaction");
+		List<String[]> queryParams1 = new ArrayList<>();
+		String[] names1 = new String[] {"name", "TEST"};
+		String[] pgm1 = new String[] {"pgm", "JUNIT"};
+		queryParams1.add(names1);
+		queryParams1.add(pgm1);
+		Response response1 = RequestUtils.postRequest(queryParams1, TRAN_PATH, ADMIN_USER, DEFAULT_PASSWORD);
+		CreateTransactionOutput ctr = RequestUtils.validateCTRSuccess(response1);
+		// Check if data is correct
+		logger.info(ctr.toString());
+		for (CreateTransaction q : ctr.getData()) {
+			assertEquals("0", q.getCc());
+			assertEquals("TEST", q.getTran());
+		}
+		for (String key : ctr.getMessages().keySet()) {
+			assertEquals(null, ctr.getMessages().get(key).getRc());
+		}
+
+		// Attempt to Delete the transaction -- Actual purpose of the function
+		logger.info("Attempting to delete test transaction");
+		List<String[]> queryParams2 = new ArrayList<>();
+		String[] names2 = new String[] {"name", "TEST"};
+		queryParams2.add(names2);
+		Response response2 = RequestUtils.deleteRequest(queryParams2, TRAN_PATH, POST_USER, DEFAULT_PASSWORD);
+		assertEquals(Response.Status.FORBIDDEN.getStatusCode(), response2.getStatus());
+		
+		// Delete the transaction
+		logger.info("Deleting test transaction");
+		List<String[]> queryParams4 = new ArrayList<>();
+		String[] names4 = new String[] {"name", "TEST"};
+		queryParams4.add(names4);
+		Response response4 = RequestUtils.deleteRequest(queryParams4, TRAN_PATH, ADMIN_USER, DEFAULT_PASSWORD);
+		DeleteTransactionOutput dtr4 = RequestUtils.validateDTRSuccess(response4);
+		// Check if data is correct
+		logger.info(dtr4.toString());
+		for (DeleteTransaction q : dtr4.getData()) {
+			assertEquals("0", q.getCc());
+			assertEquals("TEST", q.getTran());
+		}
+		for (String key : dtr4.getMessages().keySet()) {
+			assertEquals(null, dtr4.getMessages().get(key).getRc());
+		}
+		
+		// Delete program that was created
+		logger.info("Deleting test program 'JUNIT'");
+		List<String[]> queryParams3 = new ArrayList<>();
+		String[] names3 = new String[] {"name", "JUNIT"};
+		queryParams3.add(names3);
+		RequestUtils.deleteRequest(queryParams3, PGM_PATH, ADMIN_USER, DEFAULT_PASSWORD);
+	}*/
+	
+ 	
 	public void testIncorrectCredentials() {
 		
 	}
