9edd7cea1c6326035fac009dc16ffb6c62f2d4da
Joe-Winchester <winchest@uk.ibm.com>
2020-07-08 22:50:13 +0100

    Upload of working pax file
    Signed-off-by: Joe-Winchester <winchest@uk.ibm.com>

--- ims/start.sh
+++ ims/start.sh
@@ -1,23 +1,20 @@
-## This is a sample for running the .jar file on a z/OS system.  
-## Update as necessary, until we get this incorporated into the Zowe started task
-
 java -Xms16m -Xmx512m \
-	-Dapiml.service.id=zowe.org.ims.operations.api \
+	-Dapiml.service.register=true \
+	-Dapiml.service.id=IMS \
 	-Dservice.title=IMS_Operations \
 	-Dservice.description=IMS_Operations_API \
-	-Dapiml.service.port=26510 \
-	-Dapiml.service.ipAddress=9.20.5.48 \
+	-Dapiml.service.port=${ZOWE_IMS_OPS_API_PORT} \
+	-Dapiml.service.ipAddress=${ZOWE_IP_ADDRESS} \
 	-Dapiml.service.scheme=https \
-	-Dapiml.service.hostname=winmvs3b.hursley.ibm.com \
+	-Dapiml.service.hostname=${ZOWE_EXPLORER_HOST} \
 	-Dapiml.service.contextPath=/api/v1/ims \
-	-Dapiml.service.discoveryServiceUrls=https://winmvs3b.hursley.ibm.com:26501/eureka/ \
-	-Dserver.ssl.keyStore=/u/winchj/zowe-keystore-111/localhost/localhost.keystore.p12 \
-	-Dserver.ssl.keyPassword=password \
-	-Dserver.ssl.keyStorePassword=password \
-	-Dserver.ssl.trustStore=/u/winchj/zowe-keystore-111/localhost/localhost.truststore.p12 \
-	-Dserver.ssl.keyAlias=localhost \
-	-Dserver.ssl.trustStorePassword=password \
-	-Dserver.ssl.keyStoreType="PKCS12" \
-	-Dserver.ssl.trustStoreType="PKCS12" \
-	-jar zowe-ims-api-1.0.2.jar
-	
+	-Dapiml.service.discoveryServiceUrls=$https://${ZOWE_EXPLORER_HOST}:${DISCOVERY_PORT}/eureka/ \
+    -Dserver.ssl.keyStore=${KEYSTORE} \
+    -Dserver.ssl.keyStoreType=PKCS12 \
+    -Dserver.ssl.keyStorePassword=${KEYSTORE_PASSWORD} \
+    -Dserver.ssl.keyAlias=${KEY_ALIAS} \
+    -Dserver.ssl.keyPassword=${KEYSTORE_PASSWORD} \
+    -Dserver.ssl.trustStore=${TRUSTSTORE} \
+    -Dserver.ssl.trustStoreType=PKCS12 \
+    -Dserver.ssl.trustStorePassword=${KEYSTORE_PASSWORD} \
+	-jar ./ims-1.0.0-SNAPSHOT.jar
